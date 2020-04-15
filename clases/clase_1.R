f2d709eee47f541d7d39dfaa9f301766de89bf37
josé <jose@muttdata.ai>
2020-04-15 17:31:11 -0300

    first commit

---
+++ clases/clase_1.R
@@ -0,0 +1,12 @@
+autos_df <- mtcars                                                                                                
+autos_df.columns
+
+install.packages("devtools")
+devtools::install_github("tidyverse/ggplot2")
+
+load('ggplot2')
+
+# TODO hacer el ej8 para entregar el lunes
+# canal para consultas (campus?)
+# jitsi 
+# un form para respuestas?
