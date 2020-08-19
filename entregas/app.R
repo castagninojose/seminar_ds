80eb2e21eb647365c2ab18b30ccec55550ebec71
fernando <ferorti@gmail.com>
2020-08-18 23:58:27 -0300

    normal teorica

--- entregas/app.R
+++ entregas/app.R
@@ -13,9 +13,9 @@ ui <- shinyUI(fluidPage(
   
   # Sidebar with a slider input for number of observations
   sidebarLayout(
-    sidebarPanel(  # Aca van inputs, sliders etc.
-      sliderInput("obs",  # ID
-                  "N:",  # Etiqueta
+    sidebarPanel(
+      sliderInput("obs", 
+                  "N:", 
                   min = 1, 
                   max = 500, 
                   value = 300)
@@ -36,7 +36,8 @@ ui <- shinyUI(fluidPage(
     # Show a plot of the generated distribution
     mainPanel(
       plotlyOutput("distPlot"),
-      p("* El density no es la normal que queremos mostrar, aunque no se bien todavia como agregarselo al histograma de ggplot", style = "color:red"),
+      p("* En rojo es la normal teorica", style = "color:red"),
+      p("* En azul el density del histograma", style = "color:blue"),
       p("* Revisar bien rangos y que es cada valor de los sliders")
     )
   )
@@ -45,7 +46,14 @@ ui <- shinyUI(fluidPage(
 # Define server logic required to generate and plot a random distribution
 server <- shinyServer(function(input, output) {
   
-  # Funcion que ahora devuelve un objeto ggplot 
+  # Expression that generates a plot of the distribution. The expression
+  # is wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should be automatically 
+  #     re-executed when inputs change
+  #  2) Its output type is a plot 
+  #
+  
   tcl_viz <- function(n=300, sample_size=21, bins=10) {
     pe <- 0.5
     sim <- rbinom(n, sample_size, pe)
@@ -61,23 +69,39 @@ server <- shinyServer(function(input, output) {
     dominio <- seq(min(sample_means), max(sample_means), length.out=500)
     normal_teo <- dnorm(dominio, mean=sm_avg, sd=sm_sd)
     
+    hist_df <- as.data.frame(sample_means)
+    
+    normal_teo_df <- with(hist_df, data.frame(x = dominio, y =normal_teo))
+
+    fig <- ggplot(hist_df,aes(x=sample_means)) +
+      geom_histogram(aes( y = ..density..),bins=bins,
+                     binwidth = 1, fill = "grey", color = "black") + 
+      geom_density(alpha=.8, colour='dodgerblue') +
+      geom_line(data = normal_teo_df, aes(x =x, y = y), color = "red")
+
     #fig <- plot_ly(x=sample_means, type = 'histogram', histnorm = 'probability')
-    
-    
     #fig <- fig %>% add_trace(x=dominio, y=normal_teo, type='scatter')
     #fig <- fig %>% add_trace(x=sample_means, y=density, type='scatter')
     
-    fig <- ggplot(as.data.frame(sample_means), aes(x=sample_means)) + 
-      geom_histogram(aes(y=..density..),bins=bins, colour="blue", fill="dodgerblue") +
-      geom_density(alpha=.8, colour='red') + stat_function(fun=dnorm, args = list(mean=sm_avg,sd=sm_sd))#  Esta es la density de los datos, no se bien aun como agregar la otra (normal_teo)
+    
+    fig
+    
+    
+    #fig <- ggplot(as.data.frame(sample_means), aes(x=sample_means)) + 
+    #  geom_histogram(aes(y=..density..),bins=bins, colour="blue", fill="dodgerblue")+
+    #  geom_density(alpha=.8, colour='red') 
 
   }
   
   output$distPlot <- renderPlotly({
     
-    print(ggplotly(tcl_viz(input$obs,input$sample_size, input$bin))) # Como argumenos va input$ mas el id del input de la parte de server
+    # generate an rnorm distribution and plot it
+    print(ggplotly(tcl_viz(input$obs,input$sample_size, input$bin)))
   })
   
 })
 
+
+# Create Shiny app ----
 shinyApp(ui = ui, server = server)
+
